a4c097f143b657a5e50b1e75209a644bc20637bc
Brig1tte <korantengbrigitte96@gmail.com>
2024-03-11 15:42:02 +0000

    1. Don't Break Promises

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,17 +1,12 @@
-function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({status: 200, body: 'Success'});
+      resolve({
+	status: 200,
+	body: 'Success',
+      });
     } else {
-      reject(new Error('The fake API is not working currently'));
+      reject( new Error('The fake API is not working currently' ));
     }
   });
 }
-
-getFullResponseFromAPI(true).then(result => {
-  // handle the result of the asynchronous operation
-  console.log(result); // { status: 200, body: 'Success' }
-}).catch(error => {
-  // handle the error of the asynchronous operation
-  console.error(error); // Error: The fake API is not working currently
-});
