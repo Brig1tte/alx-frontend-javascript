930a05678d651248a316ffcce07377881620a494
Brig1tte <korantengbrigitte96@gmail.com>
2024-03-11 14:17:31 +0000

    1. Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-	let task = true;
-	let task2 = true;
+  if (trueOrFalse) {
+    // code here
+  }
 
-	if (trueOrFalse) {
-		task = true;
-		task2 = false;
-	}
+  const task = false;
+  const task2 = true;
 
-	return [task, task2];
+  return [task, task2];
 }
