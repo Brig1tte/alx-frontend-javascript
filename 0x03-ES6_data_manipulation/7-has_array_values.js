e432a8ca4752e8077eb4350c2e0427595a66605e
Brig1tte <korantengbrigitte96@gmail.com>
2024-03-12 13:42:51 +0000

    7. More set data structure

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,9 +1,4 @@
 export default function hasValuesFromArray(set, array) {
-  // check if input is a Set and an Array
-  if (!set instanceof Set || !Array.isArray(array)) {
-    throw new Error('Both inputs must be a Set and an Array');
-  }
-
   // iterate over the array and check if each element exists in the Set
   for (const element of array) {
     if (!set.has(element)) {
