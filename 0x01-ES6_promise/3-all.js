805e6ef0aa16fb11a93b11d82f3088fc07e312a9
Brig1tte <korantengbrigitte96@gmail.com>
2024-03-12 16:07:46 +0000

    3. Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then(
+  return Promise.all([uploadPhoto(), createUser()]).then(
     (values) => {
-      console.log(`${values[0].body} ${values[1].firstName.lastName}`);
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
     },
   ).catch(() => {
-    console.error('Signup system offline');
-    });
+    console.log('Signup system offline');
+  });
 }
