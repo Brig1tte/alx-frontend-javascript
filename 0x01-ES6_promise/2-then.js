71a32e0ed4d908233e6be9e4f8d2c5d2030ebef2
Brig1tte <korantengbrigitte96@gmail.com>
2024-03-12 15:50:50 +0000

    2. Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,19 +1,10 @@
-function handleResponseFromAPI(promise) {
-  promise
-    .then(result => {
+export default function handleResponseFromAPI(promise) {
+  promise.then((result) => {
       console.log('Got a response from the API');
       return {status: 200, body: 'success'};
     })
-    .catch(error => {
+    .catch((error) => {
       console.log('Got a response from the API');
       return new Error('No data');
     });
 }
-
-handleResponseFromAPI(getFullResponseFromAPI(true)).then(result => {
-  // handle the result of the asynchronous operation
-  console.log(result); // { status: 200, body: 'Success' }
-}).catch(error => {
-  // handle the error of the asynchronous operation
-  console.error(error); // Error: No data
-});
